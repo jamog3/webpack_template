88c0cf2f22b3e46497889983fa91a41972381944
kiwasa <iwasa@team-lab.com>
2016-12-28 01:14:01 +0900

    pugのbuild

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,17 +1,16 @@
-var path                       = require('path');
-var ExtractTextPlugin          = require('extract-text-webpack-plugin');
-var BrowserSyncPlugin          = require('browser-sync-webpack-plugin');
-var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
-
-// 開発 or build
-var isProduction = (process.env.NODE_ENV === 'production') ? true : false;
-
-// 出力path
-var destPath = isProduction ? 'build/' : '.tmp/';
-
-// cssMap
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './src/html/index.pug'
+    }),
+    new HtmlWebpackPlugin({
+      filename: './test/index.html',
+      template: './src/html/test/index.pug'
+    })
+  ]
 };
 
 module.exports = config;
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -5,25 +5,32 @@ var ExtractTextPlugin          = require('extract-text-webpack-plugin');
 var BrowserSyncPlugin          = require('browser-sync-webpack-plugin');
 var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 // 開発 or build
 var isProduction = process.argv.indexOf('-p') !== -1;
-// var isProduction = (process.env.NODE_ENV === 'production') ? true : false;
 
 // 出力path
 var destPath = isProduction ? 'build/' : '.tmp/';
 
-
 var config = merge(baseConfig, {
   devtool: 'source-map',
   entry: {
-    style: './src/stylesheets/common.sass'
+    style: './src/stylesheets/common.sass',
+    // javascript: './app.js'
   },
   output: {
-    path: path.join(__dirname, destPath + 'stylesheets'),
-    filename: '[name].css'
+    path: path.join(__dirname, destPath),
+    filename: '[name].js'
   },
   module: {
     loaders: [
+      // pug
+      {
+        test: /\.pug$/,
+        loader: 'pug'
+      },
+      // sass
       {
         test: /\.sass$/,
         loader: ExtractTextPlugin.extract('style-loader','css-loader?sourceMap!sass-loader?outputStyle=expanded&sourceMap=true&sourceMapContents=true')
@@ -32,14 +39,14 @@ var config = merge(baseConfig, {
   },
   plugins: [
     new WebpackBuildNotifierPlugin(),
-    new ExtractTextPlugin('[name].css'),
+    new ExtractTextPlugin('stylesheets/[name].css'),
     new BrowserSyncPlugin({
       host: 'localhost',
       port: 3000,
       server: { baseDir: [destPath] },
       files: [
-        '.tmp/*.css',
-        '.tmp/*.html'
+        destPath + 'stylesheets/*.css',
+        destPath + '**/*.html'
       ]
     })
   ]
