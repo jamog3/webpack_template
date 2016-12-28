9ea279cca3863be2e1d866d5d8b147d9122312bc
kiwasa <iwasa@team-lab.com>
2016-12-28 15:23:01 +0900

    htmlのファイル一覧の書式を変更

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,16 +1,54 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var fs = require('fs');
+// var path = require('path');
+
+var htmlFiles = [
+  'index',
+  'test/index'
+];
+var webpackPlugin = [];
+
+// var getFileList = function(dirPath, fileCallback, errCallback) {
+//   fs.readdir(dirPath, function(err, files) {
+//     if (err) {
+//       errCallback(err);
+//       return;
+//     }
+//     files.forEach(function(file) {
+//       var filePath = path.join(dirPath, file); // full-path
+//       if(fs.statSync(filePath).isDirectory()) {
+//         getFileList(filePath, fileCallback); // ディレクトリなら再帰
+//       } else {
+//         fileCallback(filePath); // ファイルならコールバック
+//       }
+//     });
+//   });
+// };
+
+// getFileList(__dirname+'/src/html/', function(path) {
+//   var relativePath = path.replace( __dirname+'/src/html/' , '/' );
+//   if( relativePath.match(/.pug/) && !relativePath.match(/\/_/) ) {
+//     relativePath = relativePath.replace( /.pug/g , '' );
+//     htmlFiles.push(relativePath);
+//     console.log(htmlFiles); // ファイル１つ受信
+//   }
+// }, function(err) {
+//   console.log("Receive err:" + err);
+// });
+
+htmlFiles.forEach(function(data, index, array) {
+  webpackPlugin.push(
+    new HtmlWebpackPlugin({
+      filename: './' + data + '.html',
+      template: './src/html/' + data + '.pug'
+    })
+  );
+});
 
 var config = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: './index.html',
-      template: './src/html/index.pug'
-    }),
-    new HtmlWebpackPlugin({
-      filename: './test/index.html',
-      template: './src/html/test/index.pug'
-    })
-  ]
+  plugins: webpackPlugin
 };
 
 module.exports = config;
+
+
